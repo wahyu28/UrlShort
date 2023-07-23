70cad181d39f54ce91928175598ce73ba63710f8
Wahyu Pratama <wahyulucky28@gmail.com>
2023-07-23 13:06:03 +0700

    first commit

---
+++ resources/views/layouts/guest.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <meta content="{{ csrf_token() }}" name="csrf-token">
+
+    <title>{{ config('app.name', 'URL Shortener') }}</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net" rel="preconnect">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="font-sans text-gray-900 antialiased">
+    {{ $slot }}
+</body>
+
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<x-guest-layout>
+    <div class="py-12">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                <div class="flex justify-center border-b border-gray-200 bg-white p-6">
+                    Welcome
+                </div>
+            </div>
+        </div>
+    </div>
+</x-guest-layout>
